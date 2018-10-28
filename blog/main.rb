ee48164c975da7d50cdbcdc77faffa441c58cc58
NikitaSmall <nikitasosnov@yahoo.com>
2018-10-28 14:58:18 +0200

    finish base templates

--- blog/main.rb
+++ blog/main.rb
@@ -9,22 +9,24 @@ end
 
 # list of posts: page filled from storage (in-memory pseudo-db)
 get '/posts' do
-  erb :list
+  posts = ['First post', 'Second post', 'Last post']
+  erb :list, locals: { posts: posts }
 end
 
 # страница одного поста. Берем из хранилища (почти-БД),
 # искать его будем через id (идентификатор)
 get '/posts/:id' do
-  "post ##{params[:id]}"
+  post = { id: params[:id], title: "Post ##{params[:id]}", text: 'hello world' }
+  erb :post, locals: { post: post }
 end
 
 # страница формы создания поста. Именно она отправляет запрос
 # в следующий обработчик (handler)
 get '/create/posts' do
-  'create post form'
+  erb :post_create
 end
 
 # обработчик запроса создания поста
 post '/posts' do
-  'created!'
+  redirect to('/posts')
 end
