a86f72af9f69850137c0652d9a7a0834eccc31f7
NikitaSmall <nikitasosnov@yahoo.com>
2018-11-25 12:57:43 +0200

    add example admin page for the shop

---
+++ admin/db/migrate/20181125102958_create_products.rb
@@ -0,0 +1,11 @@
+class CreateProducts < ActiveRecord::Migration[5.1]
+  def change
+    create_table :products do |t|
+      t.string :title
+      t.text :desc
+      t.float :price
+
+      t.timestamps
+    end
+  end
+end
