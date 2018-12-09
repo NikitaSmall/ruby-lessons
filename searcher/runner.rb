2cd4a0406965c4406ae80aea6ee65aeb3a4f8746
NikitaSmall <nikitasosnov@yahoo.com>
2018-12-09 13:15:17 +0200

    add duckduckgo, no code change required

--- searcher/runner.rb
+++ searcher/runner.rb
@@ -5,8 +5,7 @@ class Runner
   end
 
   def run!
-    case @options[:search_engine]
-    when 'google', 'bing'
+    if allowed_search_engines.include?(@options[:search_engine]) # 'google', 'bing'
       search(SearchPage.new(@options[:search_engine]), @options[:keyword])
     else
       raise 'unknown command!'
@@ -14,6 +13,10 @@ class Runner
   end
 
   private
+
+  def allowed_search_engines
+    Config.new.config['allowed_search_engines']
+  end
 
   def search(page, search_string)
     page.search_string(search_string)
